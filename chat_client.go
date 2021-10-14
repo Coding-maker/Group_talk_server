7c910e48c66ff5111383f71855b65df5ec0df467
gaoyuhan <1090609494@qq.com>
2021-10-14 16:42:44 +0800

    initial files

--- chat_client.go
+++ chat_client.go
@@ -8,17 +8,25 @@ import (
 	"strings"
 )
 
-// 客户端-dev
-func main1() {
-	conn, err := net.Dial("tcp", "127.0.0.1:8000")
+func main(){
+	conn, err := net.Dial("tcp", "localhost:8000")
 	if err != nil {
 		fmt.Println("err :", err)
 		return
 	}
 	defer conn.Close() // 关闭连接
 	inputReader := bufio.NewReader(os.Stdin)
+
+	go func() {
+		// 读取群聊数据
+		inputFromServer := bufio.NewScanner(conn)
+		for inputFromServer.Scan() {
+			fmt.Println(inputFromServer.Text())
+		}
+	}()
+
 	for {
-		input, _ := inputReader.ReadString(' ') // 读取用户输入
+		input, _ := inputReader.ReadString('\n') // 读取用户输入
 		inputInfo := strings.Trim(input, " \n")
 		if strings.ToUpper(inputInfo) == "Q" { // 如果输入q就退出
 			fmt.Println("连接已关闭")
@@ -28,13 +36,5 @@ func main1() {
 		if err != nil {
 			return
 		}
-		//buf := [512]byte{}
-		//n, err := conn.Read(buf[:])
-		//if err != nil {
-		//	fmt.Println("recv failed, err:", err)
-		//	return
-		//}
-		//msgFromServer := string(buf[:n])
-		//fmt.Println("收到server端发来的数据：", msgFromServer)
 	}
 }
---
+++ chat_server.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+//
+type client chan<- string // 客户端，接收消息用的通道
+var(
+	entering = make(chan client)
+	leaving = make(chan client)
+	msgs = make(chan string) // 用于告知所有的client 当前哪个client新加入聊天或离开聊天
+
+)
+
+func broadcaster(){
+	clients := make(map[client]bool)
+	for{
+		select{
+		case message := <-msgs:
+			for cli := range clients {
+				cli <- message
+			}
+		case cli := <-entering:
+			clients[cli] = true
+		case cli := <- leaving:
+			delete(clients, cli)
+			close(cli)
+		}
+	}
+}
+
+
+func handleConn(conn net.Conn){
+	ch := make(chan string) // 对外 发送客户消息 的通道
+	go clientWriter(conn, ch)
+	ch <- "type your name : "
+
+	nameReader := bufio.NewReader(conn)
+	var nameBuf [128]byte
+	n, err := nameReader.Read(nameBuf[:]) // 读取数据
+	if err != nil {
+		return
+	}
+	name := string(nameBuf[:n])
+
+	ch <- "You are :" + name
+	entering <- ch
+
+	msgs <- name + " has arrived"
+	fmt.Println(name + " has arrived")
+
+	for {
+		reader := bufio.NewReader(conn)
+		var buf [128]byte
+		n, err := reader.Read(buf[:]) // 读取数据
+		if err != nil {
+			break
+		}
+		recvStr := string(buf[:n])
+		msgs <- name + " ：" + recvStr
+		fmt.Println(name, " ：", recvStr)
+	} // 发送数据
+
+	leaving <- ch
+	msgs <- name + "  has left"
+	fmt.Println(name + "  has left")
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string){
+	for msg := range ch {
+		_, err := fmt.Fprintln(conn, msg)
+		if err != nil {
+			return 
+		}
+	}
+}
+
+
+func main(){
+	listener, err := net.Listen("tcp", "localhost:8000") // 监听端口，接受来自客户端的连接请求
+	if err != nil {
+		log.Fatal(err)
+	}
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+
